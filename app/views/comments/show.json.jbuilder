5b1b85c972efdc125ff1c45ad9bb4709410b1e93
SURESH <surisurukanti@gmail.com>
2014-02-22 13:15:48 +0530

    model added

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :product_id, :message, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Varesh::Application.config.secret_key_base = '85d967a65646e6158b53242efbb23eb320bbd566333cf94423a21ef43917d0eb3a91cc3a0750a2be3f8054b9b58eea9f01092446368e742bf04f36f9a48e9c45'
